ff8ec4ae52e116b98fafa5948228a5a275d1ca0e
赵子建 <zhaozj@htwater.net>
2018-08-13 11:52:20 +0800

    leaflet组件的简单使用

--- src/modules/leaflet.vue
+++ src/modules/leaflet.vue
@@ -1,16 +1,23 @@
 <template>
-  <htmap></htmap>
+  <htmap>
+    <l-tile-layer :url="url" layerType="overlay" name="叠加"></l-tile-layer>
+    <l-marker :lat-lng="marker"></l-marker>
+  </htmap>
 </template>
 
 <script>
-import htmap from "@/components/htmap"
+import htmap from "@/components/htmap";
+import { LTileLayer, LMarker } from '@/components/vue2leaflet';
 export default {
     name: 'leaflettest',
     components:{
-      htmap
+      htmap,LMarker,LTileLayer
     },
     data () {
-        return {}
+        return {
+          url:'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+          marker: L.latLng(29.866033,121.619373),
+        }
     },
     methods: {
     }
---
+++ src/modules/testmap.vue
@@ -0,0 +1,29 @@
+<template>
+  <mappanel>
+    数据区域ddd
+    <template slot="map">
+      <l-marker :lat-lng="marker"></l-marker>
+    </template>
+  </mappanel>
+</template>
+
+<script>
+import mappanel from "@/components/mappanel";
+import { LMarker } from '@/components/vue2leaflet';
+export default {
+    name: 'testmap',
+    components:{
+      mappanel,LMarker
+    },
+    data () {
+        return {
+          marker: L.latLng(29.866033,121.619373),
+        }
+    },
+    methods: {
+    }
+}
+</script>
+
+<style>
+</style>
